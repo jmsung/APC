aeeca7867fca4c2c3a90830b53794dda1fe1c0e1
Jongmin Sung <jongmin.sung@gmail.com>
2018-01-12 16:19:35 -0800

    Max projected intensity of movie > Find peaks from local maxima > Find molelcules from the peaks

--- SiMPull.py
+++ SiMPull.py
@@ -23,36 +23,51 @@ from __future__ import division, print_function, absolute_import
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import os
 from scipy.optimize import curve_fit
 import scipy
+from skimage.feature import peak_local_max
 
-
-class Frame(object):
-    def __init__(self, cell_name, sample_path):
-        pass
-
-   
+class Mol(object):
+    def __init__(self, I, col, row):
+        self.col = col
+        self.row = row
+        self.I_frame = I[:,col-1:col+1,row-1:row+1].sum()     
+                     
 class Movie(object):
     def __init__(self, movie_name, data_path):
         self.movie_name = movie_name
         self.movie_path = data_path + '\\' + movie_name   
         movie = Image.open(self.movie_path)
-        self.n_frame = movie.n_frames; print(self.n_frame)
-        self.row = movie.size[0]; print(self.row)
-        self.col = movie.size[1]; print(self.col)
-         
+        self.n_frame = movie.n_frames
+        self.n_row = movie.size[0]
+        self.n_col = movie.size[1]
         
-
-
-#        movie_i = Image.open(cell+'-'+ch[0][i]+'.tif')   
-#        # Read data from each frame   
-#        for j in range(self.n_frame): 
-#            movie_i.seek(j) # Move to frame j
-#            I0 = np.array(movie_i, dtype=float)
-#            self.I[i,j] = I0 - I0.min() 
-
-
+        self.I = np.zeros((self.n_frame, self.n_col, self.n_row), dtype=int)
+        for i in range(self.n_frame): 
+            movie.seek(i) # Move to i-th frame
+            self.I[i,] = np.array(movie, dtype=int)
+        
+    def bg(self):
+        I_min = np.min(self.I, axis=0)
+        for i in range(self.n_frame):
+            self.I[i,] = self.I[i,] - I_min
+            
+    def find_peaks(self): # Find local maxima from movie.I_max
+        I = self.I_max
+        self.peaks = peak_local_max(I, min_distance=1)
+        self.n_peaks = len(self.peaks[:, 1])
+        print(self.n_peaks, ' molecules')
+        
+    def find_mols(self): # Find intensity changes at peaks
+        col = self.peaks[:,1]
+        row = self.peaks[:,0]
+        self.mols = []
+        for i in range(self.n_peaks):
+            mol = Mol(self.I, col[i], row[i])
+            self.mols.append(mol)      
+                        
 class Data(object):
     def __init__(self):
         self.data_path = os.getcwd()
@@ -64,18 +79,45 @@ class Data(object):
         for movie_name in self.movie_list:
             movie = Movie(movie_name, self.data_path)
             self.movies.append(movie)
+            print(movie_name)
                                                 
     def analysis(self):
-        pass
-        
-    def plot(self):
-        pass
-                       
+        for i in range(self.movie_num):
+            movie = self.movies[i]
+            movie.bg() # Pixel-wise bg subtraction, from the minimum intensity of movie 
+            movie.I_max = np.max(movie.I, axis=0)
+            movie.find_peaks()
+            movie.find_mols()
+            
+                                               
+    def plot(self):                  
+        plt.close('all')
+        # Plot overall movie images
+        for i in range(self.movie_num):
+            movie = self.movies[i] 
+            
+            fig1, (sp1, sp2) = plt.subplots(ncols=2)      
+            sp1.imshow(movie.I_max, cmap=cm.gray)
+#            fig1.colorbar(im1, ax=sp1) 
+            sp1.set_title('Projected max intensity')
+            
+            sp2.imshow(movie.I_max, cmap=cm.gray)
+            sp2.plot(movie.peaks[:, 1], movie.peaks[:, 0], 'ro')    
+            sp2.set_title('Projected max intensity')            
 
+#            sp2.hist(movie.I_max.flatten(), bins='scott', histtype='step', color='k')  
+#            sp2.set_yscale("log") 
+#            sp2.set_title('Histogram of max intensity')
+                       
+            fig1.tight_layout()
+                      
+                                                                                                                                                           
+        plt.show()
 
 
               
 # Start  
+plt.close('all')
 data = Data()
 data.analysis()
 data.plot()
