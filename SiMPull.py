89f18a500e00e86d17400c37d23d726ed6e0cb30
Jongmin Sung <jongmin.sung@gmail.com>
2018-01-31 16:36:40 -0800

    Step finding algorithm is completed.

--- SiMPull.py
+++ SiMPull.py
@@ -54,23 +54,83 @@ class Mol(object):
         self.I_max = np.max(self.I_frame)
 
     def evaluate(self):
-        SNR = 7
-        n_signal = 7
+        self.SNR_min = 4
+        self.SNR_max = 8
+        self.dwell_min = 5
+        self.blinking = 3
         
-        signal = self.I_frame / self.noise > SNR
-        
-        if np.sum(signal[n_signal:]*signal[:-n_signal]) > 0:
-            return True
-        else: 
+        x = running_avg(self.I_frame, 3)
+        self.I_s = np.array([x[0]]+x.tolist()+[x[-1]])       
+        SNR = self.I_s / self.noise
+        signal = SNR > self.SNR_min
+
+        if np.max(SNR) < self.SNR_max:
             return False
-            
-    def find_noise(self, k):
-        n = int(k)
-        noise0 = self.I_frame[n:-n]-self.I_avg
-        noise1 = reject_outliers(noise0)
-        return np.std(noise1)        
+
+        t_b = []
+        t_ub = []
+        for i in range(len(signal)-1):
+            if (signal[i] == False) & (signal[i+1] == True):
+                t_b.append(i)
+            if (signal[i] == True) & (signal[i+1] == False):
+                t_ub.append(i)
+        
+        if len(t_b)*len(t_ub) == 0: return False 
+        if t_ub[0] < t_b[0]: # remove pre-existing binding
+            del t_ub[0]
+        if len(t_b)*len(t_ub) == 0: return False                
+        if t_ub[-1] < t_b[-1]: # remove unfinished binding
+            del t_b[-1]
+        if len(t_b)*len(t_ub) == 0: return False      
+
+        # combine blinking
+        blink_ub = []
+        blink_b = []             
+        if len(t_b) > 1:  
+            for i in range(len(t_b)-1):   
+                if abs(t_ub[i] - t_b[i+1]) <= self.blinking: 
+                    blink_ub.append(t_ub[i])
+                    blink_b.append(t_b[i+1])
+                   
+            if len(blink_ub) > 0:
+                for i in range(len(blink_ub)):
+                    t_ub.remove(blink_ub[i])
+                    t_b.remove(blink_b[i])
+
+        # delete too short binding
+        transient_ub = []
+        transient_b = []
+        for i in range(len(t_b)):                                      
+            if t_ub[i] - t_b[i] < self.dwell_min: 
+                transient_ub.append(t_ub[i])
+                transient_b.append(t_b[i])
                 
-                                                
+        if len(transient_b) > 0:
+            for i in range(len(transient_b)):
+                t_ub.remove(transient_ub[i])
+                t_b.remove(transient_b[i])
+
+        if len(t_b)*len(t_ub) == 0: return False    
+              
+        self.dwell = []     
+        self.SNR = []
+        self.I_fit = np.zeros(len(signal))          
+        for i in range(len(t_b)): 
+            self.dwell.append(t_ub[i] - t_b[i])
+            I_mean = np.mean(self.I_s[t_b[i]:t_ub[i]])
+            self.SNR.append(I_mean/self.noise)
+            self.I_fit[t_b[i]+1:t_ub[i]+1] = I_mean
+
+        return True
+
+        
+    def find_noise(self):
+        self.I_avg = running_avg(self.I_frame, 3)
+        noise0 = self.I_frame[1:-1]-self.I_avg
+        noise1 = reject_outliers(noise0)
+        self.noise = np.std(noise1)        
+
+                                         
 class Movie(object):
     def __init__(self, movie_name, data_path):
         self.movie_name = movie_name
@@ -82,8 +142,7 @@ class Movie(object):
         self.n_avg = 5
         self.frame = np.arange(self.n_frame)
         self.frame_avg = running_avg(self.frame, self.n_avg)
-        
-        
+          
         self.I = np.zeros((self.n_frame, self.n_row, self.n_col), dtype=int)
         for i in range(self.n_frame): 
             movie.seek(i) # Move to i-th frame
@@ -104,18 +163,19 @@ class Movie(object):
         row = self.peaks[::-1,0]
         col = self.peaks[::-1,1]
         self.mols = []
+        self.dwells = []
+        self.noise = []
+        self.SNR = []
         for i in range(self.n_peaks):
             mol = Mol(self.I, row[i], col[i])
-            mol.I_avg = running_avg(mol.I_frame, self.n_avg)
-            mol.noise = mol.find_noise((self.n_avg-1)/2)
-            mol.SNR = mol.I_max / mol.noise
-            if mol.evaluate():
+            mol.find_noise()
+            if mol.evaluate() is True:
                 self.mols.append(mol)    
+                self.dwells.extend(mol.dwell)
+                self.noise.append(mol.noise)
+                self.SNR.extend(mol.SNR)
         print(len(self.mols), 'molecules')  
-        
-    def find_binding(self):
-        pass
-                                        
+                                
                                                                                                                                                                                                 
 class Data(object):
     def __init__(self):
@@ -140,7 +200,6 @@ class Data(object):
             movie.I_SNR = movie.I_max / movie.I_std
             movie.find_peaks()
             movie.find_mols()
-            movie.find_binding()
             
                                                
     def plot(self):                  
@@ -159,33 +218,36 @@ class Data(object):
             sp2.imshow(movie.I_max, cmap=cm.gray)
             for j in range(n_mol):
                 sp2.plot(movie.mols[j].col, movie.mols[j].row, 'ro', markersize=2, alpha=0.5)  
-            title2 = 'Molecules = %d' % (n_mol)  
-            sp2.set_title(title2)                    
+            title = 'Molecules = %d' % (n_mol)  
+            sp2.set_title(title)                    
             fig1.tight_layout()
 
-
-            mol_Imax = []
-            mol_noise = []
-            mol_SNR = []
-            for j in range(n_mol):
-                mol_Imax.append(movie.mols[j].I_max)
-                mol_noise.append(movie.mols[j].noise)
-                mol_SNR.append(movie.mols[j].SNR)            
-
-            fig2 = plt.figure()
-            sp1 = fig2.add_subplot(221); sp1.hist(mol_Imax, bins='scott', histtype='step', color='k'); 
-            sp1.set_ylabel('Max'); 
+            fig2 = plt.figure(2)
             
-            sp2 = fig2.add_subplot(222); sp2.hist(mol_noise, bins='scott', histtype='step', color='k'); 
-            sp2.set_ylabel('noise'); 
+            sp1 = fig2.add_subplot(221)
+            sp1.hist(movie.SNR, bins='scott', histtype='step', color='k')
+            title1 = 'Mean SNR = %.1f (N = %d)' % (np.mean(movie.SNR), len(movie.SNR))
+            sp1.set_title(title1)
 
-            sp3 = fig2.add_subplot(223); sp3.hist(mol_SNR, bins='scott', histtype='step', color='k'); 
-            sp3.set_ylabel('SNR'); 
-
+            sp2 = fig2.add_subplot(222)
+            sp2.hist(movie.noise, bins='scott', histtype='step', color='k')
+            title2 = 'Mean noise = %.1f (N = %d)' % (np.mean(movie.noise), len(movie.noise))
+            sp2.set_title(title2)            
+                                    
+            sp3 = fig2.add_subplot(223)
+            sp3.hist(movie.dwells, bins='scott', histtype='step', color='k')         
+            
+            sp4 = fig2.add_subplot(224)
+            sp4.hist(movie.dwells, bins='scott', histtype='step', color='k')
+            sp4.set_yscale('log')
+            title4 = 'Mean dwell time = %.1f (N = %d)' % (np.mean(movie.dwells), len(movie.dwells))
+            sp4.set_title(title4)
+            print(title4)
+ 
             n_fig = 10
             i_fig = 1                     
-            n_col = 5
-            n_row = 4
+            n_col = 4
+            n_row = 5
             for j in range(n_mol):
                 k = j%(n_col*n_row)
                 if k == 0:
@@ -196,9 +258,11 @@ class Data(object):
                         fig = plt.figure(fig_title)
                         i_fig += 1
                 sp = fig.add_subplot(n_row, n_col, k+1)
-                sp.plot(movie.frame, movie.mols[j].I_frame, 'k', linewidth=1, markersize=3)
-                sp.axhline(y=0, color='b', linewidth=1)
-                #sp.plot(movie.frame_avg, movie.mols[j].I_avg, 'r', linewidth=3)
+                sp.plot(movie.frame, movie.mols[j].I_frame, 'k.', linewidth=0.5, markersize=3)
+                sp.plot(movie.frame, movie.mols[j].I_s, 'b', linewidth=1, markersize=3)
+                sp.plot(movie.frame, movie.mols[j].I_fit, 'r', linewidth=2, markersize=3)
+                sp.axhline(y=0, color='k', linestyle='dashed', linewidth=1)
+                sp.axhline(y=movie.mols[j].noise*movie.mols[j].SNR_min, color='k', linestyle='dashed', linewidth=1)
 
                 title_sp = '(%d, %d) (noise = %d)' % (movie.mols[j].row, movie.mols[j].col, movie.mols[j].noise)
                 sp.set_title(title_sp)
